f5d1b94328da17bfa1e29e13180755a591027ac0
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-04-27 21:01:53 +0200

    feat: Completar método matricula
    Completar código del método matrícula con un comprobante sobre
    el nombre del alumno, devolviendo true en caso de completarse
    la matrícula o false en caso contrario

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,10 +1,14 @@
 package org.formacio.component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ServeiAlumnat {
 
+	Map<Integer, String> alumnes = new HashMap<Integer, String>();
 	
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
@@ -13,7 +17,12 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
-		return true;
+		if (alumne != null) {
+			alumnes.put(id, alumne);
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
